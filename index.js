d5a3f89a2713f2fc4ba627128b7c04ba352536c1
Yaroslav Kuzyk <kuzyk.yaril@gmail.com>
2022-06-07 19:05:32 +0300

    bas

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Post from "./Post.js";
+import router from "./router.js";
+
+const PORT = 5000
+const DB_URL = "mongodb+srv://admin:admin@cluster0.inoix.mongodb.net/?retryWrites=true&w=majority"
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+startApp()
